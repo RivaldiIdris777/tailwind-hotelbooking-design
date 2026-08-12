4f10f8c383764a5f57f03f99b36a6365d98c5d55
Rivaldiidris777 <flyingriders905@gmail.com>
2026-08-12 10:46:02 +0700

    Configure static export for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  // Deploy ke GitHub Pages
+  output: "export", // build menghasilkan folder statis `out/`
+  basePath: "/tailwind-hotelbooking-design", // sesuai nama repository GitHub
+  images: {
+    unoptimized: true, // GitHub Pages tidak mendukung image optimization
+  },
 };
 
 export default nextConfig;
